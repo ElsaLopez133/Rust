246cb1a4208f8c87fd83049673e9e16ae06e5d69
Elsa <elsa.lopez.133@outlook.es>
2024-05-16 14:15:05 +0200

    Concurrency + exercise
    Learning how to use threads
    Exercise on Nodes, Trees and Subtrees.

---
+++ rust_projects/book_examples/concurrency/src/main.rs
@@ -0,0 +1,69 @@
+//To create a new thread we call the thread::spawn
+//To create channels we use mpsc::channel
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+
+fn main() {
+    /*
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    handle.join().unwrap();
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    //handle.join().unwrap();
+    */
+
+    /*
+    let v = vec![1, 2, 3];
+
+    //Using the move keyword to force a closure to take ownership of the values it uses
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+    */
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
